9922c4d96111becfe89daf6ece163b6190f2ba6a
Thanh Tu <thanhthanhvava2004@gmail.com>
2024-05-21 22:59:39 +0700

    read write file

--- src/index.js
+++ src/index.js
@@ -5,79 +5,142 @@ const port = 3000
 app.use(express.urlencoded());
 app.use(express.json())
 
-const users = [
-  {
-    'id': 1,
-    'name': 'Tú' 
-  },
-  {
-    'id': 2,
-    'name': 'Thanh' 
-  },
-  {
-    'id': 3,
-    'name': 'Nguyên' 
-  },
-  {
-    'id': 4,
-    'name': 'Nguyệt' 
-  },
-  {
-    'id': 5,
-    'name': 'Trân' 
-  }
-]
+import fs from 'node:fs' //dùng ES6 thì import thư viện như thế này, giống với const fs = require('fs')
 
 //get all users
 app.get('/users', (req, res) => {
-    console.log(users)
-    res.json(users)
+  fs.readFile('./data.json', "utf-8", (err, data) => {
+    if(err){
+      res.send("Error reading file");
+      return;
+    }
+
+    const users = JSON.parse(data);
+    res.send(users);
+  })
 })
 
 //get detail user
 app.get('/users/:id', (req, res) => {
-  const userID =  parseInt(req.params.id)
-  const detailUser = users.find(user => user.id === userID)
-  res.json(detailUser)  
+  fs.readFile('./data.json', "utf-8", (err, data) => {
+    if(err){
+      res.send("Error reading file");
+      return;
+    }
+
+    const users = JSON.parse(data);
+    
+    const index = users.findIndex(user => user.id === parseInt(req.params.id))
+    if(index == -1){
+      res.send("The id does not exist!");
+      return;
+    }
+
+    const detailUser = users.find(user => user.id === parseInt(req.params.id));
+    res.send(detailUser);
+  })
 })
 
-//post user
+//post
 app.post('/users', (req, res) => {
   // const {id, name} = req.body
   // const name = req.body.name
-  const newUser = req.body
-  console.log(newUser)
-  users.push(newUser)
-  res.json(newUser)  
+  fs.readFile('./data.json', "utf-8", (err, data) => {
+    if(err){
+      res.send("Error reading file" + err);
+      return;
+    }
+
+    const users = JSON.parse(data);
+    const newUser = req.body;
+
+    if(users.find(user => user.id === newUser.id)){
+      res.send("The id of req already exists!");
+      return;
+    }
+
+    users.push(newUser);
+    res.send(users);
+    
+    //write to file
+    fs.writeFile('./data.json', JSON.stringify(users), (e) => {
+      if(e){
+        console.log("Error writing file");
+      }else{
+        console.log("File written");
+      }
+    })
+    
+  })
 })
 
-//put user
+//put
 app.put('/users/:id', (req, res) => {
-  const userID =  parseInt(req.params.id)
-  const index = users.findIndex(user => user.id === userID)
-  if(index != -1){
-    const newUser = req.body
-    users[index] = newUser
-  }
-  res.json(users)  
+  fs.readFile('./data.json', "utf-8", (err, data) => {
+    if(err){
+      res.send("Error reading file" + err);
+      return;
+    }
+
+    const users = JSON.parse(data);
+    
+    const index = users.findIndex(user => user.id === parseInt(req.params.id))
+    if(index == -1){
+      res.send("The id does not exist!");
+      return;
+    }
+    
+    const newUser = req.body;
+    users[index] = newUser;
+    
+    res.send(users);
+
+    //write to file
+    fs.writeFile('./data.json', JSON.stringify(users), (e) => {
+      if(e){
+        console.log("Error writing file");
+      }else{
+        console.log("File written");
+      }
+    })
+  })  
 })
 
 //delete user
 app.delete('/users/:id', (req, res) => {
-  const userID =  parseInt(req.params.id)
-  const index = users.findIndex(user => user.id === userID)
-  if(index != -1){
+  fs.readFile('./data.json', "utf-8", (err, data) => {
+    if(err){
+      res.send("Error reading file" + err);
+      return;
+    }
+
+    const users = JSON.parse(data);
+    
+    const index = users.findIndex(user => user.id === parseInt(req.params.id))
+    if(index == -1){
+      res.send("The id does not exist!");
+      return;
+    }
+    
     users.splice(index, 1)
-    console.log(users)
-  }
-  res.json(users)  
+    
+    res.send(users);
+
+    //write to file
+    fs.writeFile('./data.json', JSON.stringify(users), (e) => {
+      if(e){
+        console.log("Error writing file");
+      }else{
+        console.log("File written");
+      }
+    })
+  })
 })
 
 //test => không chạy được vì bị ngăn ở trên
-app.get('/users/hello', (req, res) => {
-  res.send('hello')
-})
-
+// app.get('/users/hello', (req, res) => {
+//   res.send('hello')
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
