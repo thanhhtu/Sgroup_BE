5091fae19036f5ac08292e7a1a3a4bbcd1f8ce1b
Thanh Tu <thanhthanhvava2004@gmail.com>
2024-06-26 19:59:56 +0700

    add db

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -8,15 +8,4 @@ const pool = mysql.createPool({
 	port: 3306
 });
 
-<<<<<<< HEAD
-pool.getConnection(function (err, conn) {
-	if(err){
-		console.error('error');
-	}else{
-		console.log('oke');
-	}
-}); 
-
-=======
->>>>>>> ba34e9e9e1ef82e4717e1c67dff307b529ee1943
 export default pool;
